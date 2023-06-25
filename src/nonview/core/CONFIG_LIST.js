545d954a8b84f3864aaa9fde1df9fd8f55bf6e25
nuuuwan <nuuuwan@gmail.com>
2023-06-25 11:28:15 +0530

    Updated RAW_CONFIG_LIST

--- src/nonview/core/CONFIG_LIST.js
+++ src/nonview/core/CONFIG_LIST.js
@@ -1,4 +1,4 @@
-import RAW_CONFIG_LIST from "../../nonview/core/RAW_CONFIG_LIST";
+import { RAW_CONFIG_LIST } from "../../nonview/core/RAW_CONFIG_LIST";
 import Config from "../../nonview/core/Config";
 
 const CONFIG_LIST = RAW_CONFIG_LIST.map((d) => Config.fromRaw(d));
