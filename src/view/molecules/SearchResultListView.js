8703aad38453d4522e11f05ea348541fba31ab4d
nuuuwan <nuuuwan@gmail.com>
2023-06-25 09:21:20 +0530

    SearchResultListView

---
+++ src/view/molecules/SearchResultListView.js
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import SearchResultView from "../molecules/SearchResultView";
+
+export default function SearchResultListView({ configList }) {
+  return (
+    <Box>
+      {configList.map((config, i) => (
+        <SearchResultView key={"search-resu;t-" + i} config={config} />
+      ))}
+    </Box>
+  );
+}
