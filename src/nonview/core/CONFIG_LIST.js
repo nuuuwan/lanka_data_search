f5a8c6bd6d5e94d7f546c502a6471acb137ec6f6
nuuuwan <nuuuwan@gmail.com>
2023-06-26 17:07:30 +0530

    Sort CONFIG_LIST

--- src/nonview/core/CONFIG_LIST.js
+++ src/nonview/core/CONFIG_LIST.js
@@ -1,6 +1,8 @@
 import { RAW_CONFIG_LIST } from "../../nonview/core/RAW_CONFIG_LIST";
 import Config from "../../nonview/core/Config";
 
-const CONFIG_LIST = RAW_CONFIG_LIST.map((d) => Config.fromRaw(d));
+const CONFIG_LIST = RAW_CONFIG_LIST.map((d) => Config.fromRaw(d)).sort((a, b) =>
+  a.subCategory.localeCompare(b.subCategory)
+);
 
 export default CONFIG_LIST;
