e6e519d90bbef6a4fd5b1abe58797f25dd274a27
nuuuwan <nuuuwan@gmail.com>
2023-06-27 17:51:00 +0530

    lint

--- src/nonview/core/DataResult.js
+++ src/nonview/core/DataResult.js
@@ -42,7 +42,7 @@ export default class DataResult {
     if (label.length === 4) {
       return label + "-01-01";
     }
- 
+
     for (let i = 0; i < MONTHS.length; i++) {
       const month = MONTHS[i];
       if (label.endsWith(month)) {
