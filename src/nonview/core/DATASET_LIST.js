da19f398dd734ae5f8f1791623660f3fee86538b
nuuuwan <nuuuwan@gmail.com>
2023-06-27 20:26:08 +0530

    Added DATASET_IDX

--- src/nonview/core/DATASET_LIST.js
+++ src/nonview/core/DATASET_LIST.js
@@ -5,4 +5,9 @@ const DATASET_LIST = RAW_DATASET_LIST.map((d) => Dataset.fromRaw(d))
   .filter((dataset) => dataset.isValidForVisualization())
   .sort((a, b) => a.subCategory.localeCompare(b.subCategory));
 
+export const DATASET_IDX = DATASET_LIST.reduce(function (idx, dataset) {
+  idx[dataset.key] = dataset;
+  return idx;
+}, {});
+
 export default DATASET_LIST;
