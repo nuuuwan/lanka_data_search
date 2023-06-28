45c6a52f9edf44f5837d434477a4e4c7b6b2b1c3
nuuuwan <nuuuwan@gmail.com>
2023-06-28 13:23:44 +0530

    Use TopMenu

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -10,12 +10,11 @@ import Stack from "@mui/material/Stack";
 import RandomX from "../../nonview/utils/RandomX";
 import URLContext from "../../nonview/utils/URLContext";
 import IconButton from "@mui/material/IconButton";
-import CasinoIcon from "@mui/icons-material/Casino";
 import Tooltip from "@mui/material/Tooltip";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import { Helmet } from "react-helmet";
 import AlertDatasets from "../atoms/AlertDatasets";
 import AlertCBSLApp from "../atoms/AlertCBSLApp";
+import TopMenu from "../molecules/TopMenu";
 
 const N_DISPLAY_START = 1;
 export default class HomePage extends Component {
@@ -60,44 +59,6 @@ export default class HomePage extends Component {
     this.setState({ datasetList });
   }
 
-  renderMenu() {
-    const onClickRandom = function () {
-      URLContext.setContext({ datasetKeyList: undefined });
-      window.location.reload();
-    };
-
-    const onClickTweet = function () {
-      const { datasetList } = this.state;
-      const dataSetText = datasetList.map((x) => x.tweetText).join("\n");
-      const tweetText = [
-        dataSetText,
-        "",
-        "#LankaDataSearch by @nuuuwan",
-        "",
-        window.location.href,
-      ].join("\n");
-      const tweetURL =
-        "https://twitter.com/intent/tweet?text=" +
-        encodeURIComponent(tweetText);
-      window.open(tweetURL, "_blank");
-    }.bind(this);
-
-    return (
-      <>
-        <Tooltip title="Open random datasets">
-          <IconButton onClick={onClickRandom}>
-            <CasinoIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Tweet this chart">
-          <IconButton onClick={onClickTweet}>
-            <TwitterIcon />
-          </IconButton>
-        </Tooltip>
-      </>
-    );
-  }
-
   renderSources() {
     return DATA_SOURCE_LIST.map(function (dataSource) {
       const onClick = function () {
@@ -117,7 +78,7 @@ export default class HomePage extends Component {
     return (
       <Stack direction="row">
         {this.renderSources()}
-        {this.renderMenu()}
+        <TopMenu />
       </Stack>
     );
   }
