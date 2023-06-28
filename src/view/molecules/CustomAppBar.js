e657289f6598b6f527c5d95d789fe78099215d3b
nuuuwan <nuuuwan@gmail.com>
2023-06-28 21:34:17 +0530

    Added N_DATASET_LIST to title

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -2,13 +2,15 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { N_DATASET_LIST } from "../../nonview/core/DATASET_LIST";
 
 export default function CustomAppBar() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Lanka Data Search
+          Lanka Data Search ({N_DATASET_LIST.toLocaleString()}{" "}
+          <span style={{ fontSize: "75%" }}>Datasets</span>)
         </Typography>
       </Toolbar>
     </AppBar>
