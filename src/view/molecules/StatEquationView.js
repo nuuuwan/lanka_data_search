b1678216e42d6084e77da3f1d30a297f27af2599
nuuuwan <nuuuwan@gmail.com>
2023-06-28 09:07:18 +0530

    Added "where A = " etc

--- src/view/molecules/StatEquationView.js
+++ src/view/molecules/StatEquationView.js
@@ -2,17 +2,20 @@ import React from "react";
 import DataResult from "../../nonview/core/DataResult";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import Alert from "@mui/material/Alert";
+const SYMBOL1 = "α";
+const SYMBOL2 = "β";
 
 function equationText(m, c, order) {
-  const y = order ? "d₂" : "d₁";
-  const x = order ? "d₁" : "d₂";
+  const y = order ? SYMBOL2 : SYMBOL1;
+  const x = order ? SYMBOL1 : SYMBOL2;
 
   return `${y} = ${m.toPrecision(4)}·${x} ${c >= 0 ? "+" : "-"} ${Math.abs(
     c
   ).toPrecision(4)}`;
 }
 
-export default function StatEquationView({ dataResultList }) {
+export default function StatEquationView({ dataResultList, datasetList }) {
   if (dataResultList.length !== 2) {
     return null;
   }
@@ -24,6 +27,9 @@ export default function StatEquationView({ dataResultList }) {
     dataResultList[1],
     dataResultList[0]
   );
+
+  const label1 = datasetList[0].subCategory;
+  const label2 = datasetList[1].subCategory;
   return (
     <Paper
       elevation={0}
@@ -35,9 +41,19 @@ export default function StatEquationView({ dataResultList }) {
         width: 300,
       }}
     >
-      <Typography variant="caption">Line Equations</Typography>
+      <Typography variant="caption">Relationship between Datasets</Typography>
       <Typography variant="h6">{equationText(m1, c1, true)}</Typography>
       <Typography variant="h6">{equationText(m2, c2, false)}</Typography>
+      <Typography variant="body1">where</Typography>
+      <Typography variant="body1">
+        {SYMBOL1} = "{label1}"
+      </Typography>
+      <Typography variant="body1">
+        {SYMBOL2} = "{label2}"
+      </Typography>
+      <Alert severity="warning" sx={{ margin: 1 }}>
+        Assuming the relationship is linear.
+      </Alert>
     </Paper>
   );
 }
