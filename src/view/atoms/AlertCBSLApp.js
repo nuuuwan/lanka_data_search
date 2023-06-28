c67e28d9f3adf2af7b0380b1732252ffb3fee4c2
nuuuwan <nuuuwan@gmail.com>
2023-06-28 16:59:02 +0530

    Added details to CBSL message

--- src/view/atoms/AlertCBSLApp.js
+++ src/view/atoms/AlertCBSLApp.js
@@ -1,15 +1,21 @@
 import * as React from "react";
 import Alert from "@mui/material/Alert";
 import Link from "@mui/material/Link";
-
+import Box from "@mui/material/Box";
 export default function AlertCBSLApp() {
   return (
     <Alert severity="info" sx={{ margin: 1 }}>
-      This app was formerly{" "}
-      <Link href="https://nuuuwan.github.io/cbsl_app">
-        https://nuuuwan.github.io/cbsl_app
-      </Link>
-      .
+      <Box>
+        This app was formerly{" "}
+        <Link href="https://nuuuwan.github.io/cbsl_app">
+          https://nuuuwan.github.io/cbsl_app
+        </Link>
+        , which now redirects to this app.
+      </Box>
+      <Box>
+        This updated app contains from the Central Bank of Sri Lanka, as well as
+        other Public Data sources related to Sri Lanka.
+      </Box>
     </Alert>
   );
 }
