31c232b8cf0082ca657aa64a002a0caceb5d5bfa
nuuuwan <nuuuwan@gmail.com>
2023-06-28 21:02:07 +0530

    FIX CONFIG in scripts_py BUG

--- scripts_py/generate_raw_datase_list.sh
+++ scripts_py/generate_raw_datase_list.sh
@@ -1,5 +1,5 @@
 rm  C:\Users\ASUS\AppData\Local\Temp\www.979dd029.json
 rm C:\Users\ASUS\AppData\Local\Temp\www.30fa5969.json
-python .\scripts_py\generate_raw_config_list.py
+python .\scripts_py\generate_raw_datase_list.py
 jli
 echo "Done"
