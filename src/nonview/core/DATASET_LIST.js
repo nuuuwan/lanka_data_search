f9ab5297f926f47335e03c2f791f366659b065ae
nuuuwan <nuuuwan@gmail.com>
2023-06-28 21:34:09 +0530

    Added N_DATASET_LIST

--- src/nonview/core/DATASET_LIST.js
+++ src/nonview/core/DATASET_LIST.js
@@ -5,6 +5,8 @@ const DATASET_LIST = RAW_DATASET_LIST.map((d) => Dataset.fromRaw(d))
   .filter((dataset) => dataset.isValidForVisualization())
   .sort((a, b) => a.subCategory.localeCompare(b.subCategory));
 
+export const N_DATASET_LIST = DATASET_LIST.length;
+
 export const DATASET_IDX = DATASET_LIST.reduce(function (idx, dataset) {
   idx[dataset.key] = dataset;
   return idx;
