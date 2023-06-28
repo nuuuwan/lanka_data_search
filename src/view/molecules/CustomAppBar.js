d35b00ecc21e126296cfd6d78886ed450d213582
nuuuwan <nuuuwan@gmail.com>
2023-06-28 17:35:36 +0530

    Removed sources from AppBar

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -3,8 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import TopMenu from "../molecules/TopMenu";
-import DataSourceMenuButton from "../atoms/DataSourceMenuButton";
-import { DATA_SOURCE_LIST } from "../../nonview/core/DATA_SOURCE_IDX";
 
 export default function CustomAppBar({ datasetList, refChart }) {
   return (
@@ -14,14 +12,6 @@ export default function CustomAppBar({ datasetList, refChart }) {
           Lanka Data Search
         </Typography>
 
-        {DATA_SOURCE_LIST.map(function (dataSource) {
-          return (
-            <DataSourceMenuButton
-              key={"data-source-menu-item-" + dataSource.id}
-              dataSource={dataSource}
-            />
-          );
-        })}
         <TopMenu datasetList={datasetList} refChart={refChart} />
       </Toolbar>
     </AppBar>
