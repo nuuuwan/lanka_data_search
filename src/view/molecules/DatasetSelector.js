9873a11f1e8480d0a429af27430237371b858a92
nuuuwan <nuuuwan@gmail.com>
2023-06-30 20:11:04 +0530

    Updated filterOptions for better search

--- src/view/molecules/DatasetSelector.js
+++ src/view/molecules/DatasetSelector.js
@@ -16,10 +16,17 @@ export default function DatasetSelector({
     onChangeDatasetList(datasetList);
   };
 
+  const filterOptions = function(allDatasetList, state) {
+    return allDatasetList.filter((dataset) => {
+      return dataset.isMatch(state.inputValue)
+    });
+  }
+
   return (
     <Box sx={{ margin: 2 }}>
       <Autocomplete
         multiple
+        filterOptions={filterOptions}
         options={allDatasetList}
         value={selectedDatasetList}
         getOptionLabel={(dataset) => renderDataset(dataset)}
