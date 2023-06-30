5682207efbdb82a235dd4d502b9541d3dfa614df
nuuuwan <nuuuwan@gmail.com>
2023-06-30 11:22:32 +0530

    lint

--- src/view/atoms/AlertCBSLApp.js
+++ src/view/atoms/AlertCBSLApp.js
@@ -5,16 +5,17 @@ import Box from "@mui/material/Box";
 export default function AlertCBSLApp() {
   return (
     <Alert severity="info" sx={{ margin: 1 }}>
-      <Box sx={{  padding: 1 }}>
-        This app was formerly {" "}
+      <Box sx={{ padding: 1 }}>
+        This app was formerly{" "}
         <Link href="https://nuuuwan.github.io/cbsl_app">
           https://nuuuwan.github.io/cbsl_app
-        </Link>.
+        </Link>
+        .
       </Box>
-      <Box sx={{  padding: 1 }}>
-        This updated app contains data from the Central Bank of Sri Lanka, 
-        as well as other Public Data sources related to Sri Lanka,
-        including the World Bank.
+      <Box sx={{ padding: 1 }}>
+        This updated app contains data from the Central Bank of Sri Lanka, as
+        well as other Public Data sources related to Sri Lanka, including the
+        World Bank.
       </Box>
     </Alert>
   );
