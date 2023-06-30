3693757ad776e818db38851477fb74b5a1a7af9b
nuuuwan <nuuuwan@gmail.com>
2023-06-30 14:01:01 +0530

    lint

--- src/view/atoms/AlertCBSLApp.js
+++ src/view/atoms/AlertCBSLApp.js
@@ -3,15 +3,12 @@ import Alert from "@mui/material/Alert";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import DataSourceLink from "./DataSourceLink";
-import {DATA_SOURCE_LIST} from "../../nonview/core/DATA_SOURCE_IDX";
+import { DATA_SOURCE_LIST } from "../../nonview/core/DATA_SOURCE_IDX";
 
 export default function AlertCBSLApp() {
-
-  const renderedSources = DATA_SOURCE_LIST.map(
-    function (dataSource) {
-      return <DataSourceLink dataSource={dataSource} />
-    }
-  )
+  const renderedSources = DATA_SOURCE_LIST.map(function (dataSource) {
+    return <DataSourceLink dataSource={dataSource} />;
+  });
 
   return (
     <Alert severity="info" sx={{ margin: 1 }}>
@@ -23,7 +20,7 @@ export default function AlertCBSLApp() {
         .
       </Box>
       <Box sx={{ padding: 1 }}>
-        This updated app contains data from the following sources: 
+        This updated app contains data from the following sources:
         {renderedSources}.
       </Box>
     </Alert>
