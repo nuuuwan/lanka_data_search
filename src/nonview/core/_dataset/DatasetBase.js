4204e0378ba364bbad203008ab076e37ae7ad0c8
nuuuwan <nuuuwan@gmail.com>
2023-08-02 13:40:06 +0530

    Split DatabaseBaseProps

--- src/nonview/core/_dataset/DatasetBase.js
+++ src/nonview/core/_dataset/DatasetBase.js
@@ -1,31 +1,31 @@
 export default class DatasetBase {
-    constructor(
-      sourceID,
-      category,
-      subCategory,
-      scale,
-      unit,
-      frequencyName,
-      iSubject,
-      footnotes,
-      n,
-      minT,
-      maxT,
-      minValue,
-      maxValue
-    ) {
-      this.sourceID = sourceID;
-      this.category = category;
-      this.subCategory = subCategory;
-      this.scale = scale;
-      this.unit = unit;
-      this.frequencyName = frequencyName;
-      this.iSubject = iSubject;
-      this.footnotes = footnotes;
-      this.n = n;
-      this.minT = minT;
-      this.maxT = maxT;
-      this.minValue = minValue;
-      this.maxValue = maxValue;
-    }
+  constructor(
+    sourceID,
+    category,
+    subCategory,
+    scale,
+    unit,
+    frequencyName,
+    iSubject,
+    footnotes,
+    n,
+    minT,
+    maxT,
+    minValue,
+    maxValue
+  ) {
+    this.sourceID = sourceID;
+    this.category = category;
+    this.subCategory = subCategory;
+    this.scale = scale;
+    this.unit = unit;
+    this.frequencyName = frequencyName;
+    this.iSubject = iSubject;
+    this.footnotes = footnotes;
+    this.n = n;
+    this.minT = minT;
+    this.maxT = maxT;
+    this.minValue = minValue;
+    this.maxValue = maxValue;
+  }
 }
