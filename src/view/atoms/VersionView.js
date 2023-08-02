2f670a934cec1a286a2ce2d10dd59b165e56cd18
nuuuwan <nuuuwan@gmail.com>
2023-08-02 12:15:49 +0530

    Added padding to VersionView

--- src/view/atoms/VersionView.js
+++ src/view/atoms/VersionView.js
@@ -2,7 +2,7 @@ import { Typography, Box, Link } from "@mui/material";
 import { VERSION, VERSION_INFO } from "../../nonview/core/Version";
 export default function VersionView() {
   return (
-    <Box sx={{ margin: 1, padding: 1, textAlign: "center" }}>
+    <Box sx={{ margin: 2, padding: 2, textAlign: "center" }}>
       <Typography variant="body2">
         {VERSION} by{" "}
         <Link href="https://www.github.com/nuuuwan/" target="_blank">
