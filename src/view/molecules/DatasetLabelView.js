3211c72c137181b313465b431344ea7786c4f4db
nuuuwan <nuuuwan@gmail.com>
2023-08-02 18:19:49 +0530

    typo

--- src/view/molecules/DatasetLabelView.js
+++ src/view/molecules/DatasetLabelView.js
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import DataSourceAvatar from "../atoms/DataSourceAvatar";
-export default function DatasetDetailsView({ dataset }) {
+export default function DatasetLabelView({ dataset }) {
   return (
     <Box>
       <DataSourceAvatar dataSource={dataset.source} />
