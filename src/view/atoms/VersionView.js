590512dc057291a15f07dfc3016291d40bcfe5dd
nuuuwan <nuuuwan@gmail.com>
2023-08-05 17:42:56 +0530

    Thicker Margin

--- src/view/atoms/VersionView.js
+++ src/view/atoms/VersionView.js
@@ -6,7 +6,7 @@ import ButtonReportBug from "./ButtonReportBug";
 import { VERSION, VERSION_INFO } from "../../nonview/core/Version";
 export default function VersionView() {
   return (
-    <Box sx={{ margin: 2, padding: 2, textAlign: "center" }}>
+    <Box sx={{ marginTop: 10, padding: 2, textAlign: "center" }}>
       <Typography variant="body2">
         {VERSION} by{" "}
         <Link href="https://www.github.com/nuuuwan/" target="_blank">
