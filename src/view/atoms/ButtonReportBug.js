d676c7b3aa3ca107003e30e93f3cbdce4046628a
nuuuwan <nuuuwan@gmail.com>
2023-08-05 15:35:38 +0530

    lint

--- src/view/atoms/ButtonReportBug.js
+++ src/view/atoms/ButtonReportBug.js
@@ -1,13 +1,13 @@
 import React from "react";
-import BugReportIcon from '@mui/icons-material/BugReport';
+import BugReportIcon from "@mui/icons-material/BugReport";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 
-const URL_ISSUES = 'https://github.com/nuuuwan/lanka_data_search/issues/new'
+const URL_ISSUES = "https://github.com/nuuuwan/lanka_data_search/issues/new";
 
 export default function ButtonReportBug() {
   const onClick = function () {
-    window.open(URL_ISSUES, '_blank');
+    window.open(URL_ISSUES, "_blank");
   };
   return (
     <Tooltip title="Report Bug">
