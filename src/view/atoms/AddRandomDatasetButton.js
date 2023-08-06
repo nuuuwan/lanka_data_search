c8aa6a23d5b0906c564229b29e9ed5269300e523
nuuuwan <nuuuwan@gmail.com>
2023-08-06 13:00:46 +0530

    typo

--- src/view/atoms/AddRandomDatasetButton.js
+++ src/view/atoms/AddRandomDatasetButton.js
@@ -4,7 +4,7 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 export default function AddRandomDatasetButton({ onClickRandom }) {
   return (
-    <Tooltip title="Add Random Dataset">
+    <Tooltip title="Add Random Datasets">
       <IconButton onClick={onClickRandom} color="primary">
         <CasinoIcon />
       </IconButton>
