696c7b12ecc1ff268bdbffc62a5543f1d9c3f63b
nuuuwan <nuuuwan@gmail.com>
2025-02-24 14:42:00 +0530

    Updated message

--- src/view/atoms/VersionView.js
+++ src/view/atoms/VersionView.js
@@ -1,19 +1,20 @@
-import { Typography, Box, Link } from "@mui/material";
-import ButtonRefresh from "./ButtonRefresh";
-import Grid from "@mui/material/Grid";
-import ButtonReportBug from "./ButtonReportBug";
+import { Typography, Box, Link } from '@mui/material';
+import ButtonRefresh from './ButtonRefresh';
+import Grid from '@mui/material/Grid';
+import ButtonReportBug from './ButtonReportBug';
 
-import { VERSION } from "../../nonview/constants";
+import { VERSION } from '../../nonview/constants';
 
 export default function VersionView() {
   return (
-    <Box sx={{ marginTop: 10, padding: 2, textAlign: "center" }}>
+    <Box sx={{ marginTop: 10, padding: 2, textAlign: 'center' }}>
       <Typography variant="body2">
-        {"Last update at " + VERSION.DATETIME_STR + " by "}
+        {'The App was Last Updated at ' + VERSION.DATETIME_STR + ' by '}
         <Link href="https://www.github.com/nuuuwan/" target="_blank">
           nuuuwan
         </Link>
-      </Typography>
+      </Typography>{' '}
+      <Typography variant="body2">Data is updated hourly.</Typography>
       <Grid container justifyContent="center">
         <ButtonRefresh />
         <ButtonReportBug />
